0fc00c3c786c83c96e5ca9c5774e1ba69a63a919
Emin Mirzoev <m.emin123@live.com>
2019-10-22 03:41:27 +0400

    Homeworks

--- src/Ilist.js
+++ src/Ilist.js
@@ -1,14 +1,14 @@
 function IList() {}
 
-IList.prototype.clear = function() {console.log('IList init'); }
-IList.prototype.toString = function() {console.log('IList init'); }
-IList.prototype.getSize = function() {console.log('IList init'); }
-IList.prototype.getSort = function() {console.log('IList init'); }
-IList.prototype.push = function() {console.log('IList init'); }
-IList.prototype.pop = function() {console.log('IList init'); }
-IList.prototype.shift = function() {console.log('IList init'); }
-IList.prototype.unshift = function() {console.log('IList init'); }
-IList.prototype.getSplice = function() {console.log('IList init'); }
-IList.prototype.getSlice = function() {console.log('IList init'); }
+IList.prototype.clear = function() {console.log('IList clear'); }
+IList.prototype.toString = function() {console.log('IList toString'); }
+IList.prototype.getSize = function() {console.log('IList getSize'); }
+IList.prototype.getSort = function() {console.log('IList getSort'); }
+IList.prototype.push = function() {console.log('IList push'); }
+IList.prototype.pop = function() {console.log('IList pop'); }
+IList.prototype.shift = function() {console.log('IList shift'); }
+IList.prototype.unshift = function() {console.log('IList unshift'); }
+IList.prototype.getSplice = function() {console.log('IList getSplice'); }
+IList.prototype.getSlice = function() {console.log('IList getSlice'); }
 
 module.exports = IList;
